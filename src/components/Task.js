2bfbda677dfeb08b2c36bff7a0285347dea9bc52
Gary Gatchalian <garyandrewgatchalian@gmail.com>
2022-12-11 21:43:58 +0800

    initial

---
+++ src/components/Task.js
@@ -0,0 +1,22 @@
+// import React from 'react';
+import { FaCheckDouble, FaRegTrashAlt, FaEdit } from 'react-icons/fa';
+// import TaskList from './TaskList';
+// import { GrCheckboxSelected } from 'react-icons/fa';
+
+const Task = ({ task, index, deleteTask, getSingleTask, setToComplete }) => {
+  return (
+    <div className={task.completed ? 'task completed' : 'task'}>
+      <p>
+        <b>{index + 1}. </b>
+        {task.name}
+      </p>
+      <div className='task-icons'>
+        <FaCheckDouble color='green' onClick={() => setToComplete(task)} />
+        <FaEdit color='purple' onClick={() => getSingleTask(task)} />
+        <FaRegTrashAlt color='red' onClick={() => deleteTask(task._id)} />
+      </div>
+    </div>
+  );
+};
+
+export default Task;
