20a51c700ee307bab8f531477e3b129f1018fe7b
Gary Gatchalian <garyandrewgatchalian@gmail.com>
2022-12-21 10:18:39 +0800

    about page

--- src/About.js
+++ src/About.js
@@ -17,6 +17,13 @@ function About() {
           </h3>
           <p className='fst-italic about-container'>"Makes your life easier and accomplish more today."</p>
         </div>
+        {/* <p className='mx-5 lh-base pt-5'>
+          <span className='text-primary'> Unote</span> is a web application it has a dedicated space for clients to
+          manage their tasks. You can just simply add, edit and remove your specific task. It has a simple, no-frills
+          interface built around a classic design. This task-tracking site is great for individuals, though they’ve
+          recently expanded into teams and adapted to small business needs. However, they still fall short when it comes
+          to team management, as you can’t assign multiple people to a single task.
+        </p> */}
         <div className='members py-2 px-3 pt-5 about-container'>
           <p>Paulo Santiago</p>
           <p>John Andrei Ignacio</p>
